5083a4ad98a0f69381d91380bb71af163f62128f
sarakhaghani <sskkmilani@gmail.com>
2023-03-19 21:20:19 +0330

    added JavaScript

--- src/app.js
+++ src/app.js
@@ -1,1 +1,37 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
 
+function displayTemp(response) {
+  console.log(response.data);
+  let cityElement = document.querySelector("#city");
+  let currentTempElement = document.querySelector("#currentTemp");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
+  currentTempElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = Math.round(response.data.main.humidity);
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+}
+
+let apiKey = "42ab2c2ee73f7e1a2ff24479d6688080";
+apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Tehran&units=metric&appid=${apiKey}`;
+
+axios.get(apiUrl).then(displayTemp);
